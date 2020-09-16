06a2668bbfd2879ca1814bea8d826b6430fa3baa
CursevonReiki@outlook.com <CursevonReiki@outlook.com>
2020-09-16 19:29:58 +0300

    Up

--- DSRPG/GameLogic/Hero/HeroBase.cs
+++ DSRPG/GameLogic/Hero/HeroBase.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Management.Instrumentation;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -14,11 +15,13 @@ namespace DSRPG.GameLogic.Hero
         protected string name;
         protected int health;
         protected int mana;
+        protected int energy;
         protected int strength;
         protected int agility;
         protected int stamina;
         protected int intellect;
-        protected int armor;
+        protected double armor;
+        protected int damage;
 
         public string Name
         {
@@ -34,6 +37,12 @@ namespace DSRPG.GameLogic.Hero
         {
             get { return mana; }
             protected set { mana = value; }
+        }
+        
+        public int Energy
+        {
+            get { return energy; }
+            set { energy = value; }
         }
         public int Strength
         {
@@ -55,12 +64,16 @@ namespace DSRPG.GameLogic.Hero
             get { return intellect; }
             protected set { intellect = value; }
         }
-        public int Armor
+        public double Armor
         {
             get { return armor; }
             protected set { armor = value; }
         }
-
+        public int Damage
+        {
+            get { return damage; }
+            set { damage = value; }
+        }
         protected HeroBase()
         {
             Name = name;
@@ -73,6 +86,41 @@ namespace DSRPG.GameLogic.Hero
             Armor = armor;
         }
 
+
+        public int UpHealth()
+        {
+            health = health + (stamina * 5);
+            return health;
+        } // хп больше от стамины
+        public int UpMana()
+        {
+            mana = mana + (intellect * 5);
+            return mana;
+        } // маны больше от интелекта
+
+        public int UpEnergy()
+        {
+            if(agility % 3 == 0)
+            {
+                energy = energy + (agility * 1);
+            }
+            return energy;
+        } // если число кратно 3 а то есть каждые 3 очка повышается энергия от ловкости
+
+        public int UpDamage()
+        {
+            if(strength % 2 == 0)
+            {
+                damage = damage + (strength * 1);
+            }
+            return damage;
+        } // если число кратно 2 а то есть каждые 2 очка повышается дамага от силы
+
+        public double UpArmor()
+        {
+            armor = armor + ((strength * 0.1)/100);
+            return armor;
+        }  // повышение армора от силы 
         public bool CheckDie()
         {
             if (health == 0)
@@ -83,17 +131,18 @@ namespace DSRPG.GameLogic.Hero
             {
                 return false;
             }
-        }
+        }  // чек на смерть
         public bool CheckNoneMana()
         {
             if (mana == 0) return true;
             else return false;
-        }
+        } // чек на отсуствие маны
         
         public void CheckResist()
         {
-          //  int reset = damagemob * (1-(armor/100));
-        }
+          // int reset = damagemob * (1-(armor/100));
+        } // доделать 
+
         
         public override string ToString() //Мож пойже пригодится
         {
--- DSRPG/UI/CreateHeroPageUI/NamePage.xaml.cs
+++ DSRPG/UI/CreateHeroPageUI/NamePage.xaml.cs
@@ -23,11 +23,42 @@ namespace DSRPG.UI.CreateHeroPageUI
         public NamePage()
         {
             InitializeComponent();
+            Name.MaxLength = 10;
         }
 
         public string GetResult()
         {
             return Name.Text.ToString();
+        }
+
+        private void Name_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (Name.Text == "")
+            {
+                Name.Text = "Введите имя";
+                Name.Foreground = Brushes.White;
+            }
+        }
+
+        private void Name_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (Name.Text == "Введите имя")
+            {
+                Name.Text = "";
+                Name.Foreground = Brushes.AntiqueWhite;
+            }
+        }
+
+        private void Name_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            char q = Convert.ToChar(e.Text);
+            string s = e.Text;
+            byte[] bytes = Encoding.GetEncoding(1251).GetBytes(s);
+            if (q <= 97 && q <= 64 && Convert.ToChar(s) <= 191 || q >= 123 && q >= 91 && Convert.ToChar(s) <= 256)
+            {
+                e.Handled = true;
+                MessageBox.Show("Ввод только символов !", "Ошибка");
+            }
         }
     }
 }
