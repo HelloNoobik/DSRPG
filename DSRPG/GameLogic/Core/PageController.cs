7e6f739ad973888db197f51838244a6552d2f2da
HelloNoobik <HelloNoobik@outlook.com>
2020-09-17 20:42:28 +0300

    Конфликты

---
+++ DSRPG/GameLogic/Core/PageController.cs
@@ -0,0 +1,148 @@
+﻿using DSRPG.Test;
+using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace DSRPG
+{
+    public enum Pages
+    {
+        Main,
+        Intro,
+        Settings,
+        BattleArena,
+        CreateCharacter,
+        Lotrik,
+        WorldMap,
+    }
+}
+
+namespace DSRPG.GameLogic.Core
+{
+    public class PageController : ViewModelBase, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        private Page Main;
+        private Page Intro;
+        private Page Settings;
+        private Page BattleArena;
+        private Page CreateCharacter;
+        private Page Lotrik;
+        private Page WorldMap;
+        private Page Test;
+
+        private Page currentPage;
+     
+        public Page CurrentPage
+        {
+            get
+            {
+                return currentPage;
+            }
+
+            set
+            {
+                if (value == currentPage) return;
+                currentPage = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private double pageOpacity;
+        public double PageOpacity
+        {
+            get
+            {
+                return pageOpacity;
+            }
+
+            set
+            {
+                if (value >= 0.0 && value <= 1.0) pageOpacity = value;
+                else if (value < 0.0) pageOpacity = 0.0;
+                else pageOpacity = 1.0;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public PageController()
+        {
+            Main = new UI.MainMenu();
+            Intro = new UI.Intro();
+            Settings = new UI.SettingsPage();
+            BattleArena = new UI.BattleArena();
+            CreateCharacter = new UI.CreateCharacter();
+            Lotrik = new UI.Lotrik();
+            WorldMap = new UI.WorldMap();
+            Test = new UI.HeroPage();
+
+            CurrentPage = Main;
+
+            PageOpacity = 1.0;
+        }
+
+        private async void SlowOpacity(Page page)
+        {
+            await Task.Factory.StartNew(() =>
+            {
+                for (double i = 1.0; i > 0.0; i -= 0.1)
+                {
+                    PageOpacity = i;
+                    Thread.Sleep(10);
+                }
+
+                CurrentPage = page;
+
+                for (double i = 0.0; i < 1.0; i += 0.1)
+                {
+                    PageOpacity = i;
+                    Thread.Sleep(10);
+                }
+            });
+        }
+
+        public void ChangeWindow(Pages page)
+        {
+            Page _page = null;
+            switch (page)
+            {
+                case Pages.Main:
+                    _page = Main;
+                    break;
+                case Pages.Intro:
+                    _page = Intro;
+                    break;
+                case Pages.Settings:
+                    _page = Settings;
+                    break;
+                case Pages.BattleArena:
+                    _page = BattleArena;
+                    break;
+                case Pages.CreateCharacter:
+                    _page = CreateCharacter;
+                    break;
+                case Pages.Lotrik:
+                    _page = Lotrik;
+                    break;
+                case Pages.WorldMap:
+                    _page = WorldMap;
+                    break;
+            }
+            SlowOpacity(_page);
+        }
+
+    }
+}
---
+++ DSRPG/UI/Intro.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using DSRPG.GameLogic.Core;
+
+namespace DSRPG.UI
+{
+    /// <summary>
+    /// Логика взаимодействия для Intro.xaml
+    /// </summary>
+    public partial class Intro : Page
+    {
+        public Intro()
+        {
+            InitializeComponent();
+        }
+
+        private void Page_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key.ToString() == "Return")
+            {
+                Player.Close();
+                Settings.PageController.ChangeWindow(Pages.WorldMap);
+                Window.GetWindow(this).KeyDown -= Page_KeyDown;
+            }
+        }
+
+        private void media_MediaEnded(object sender, RoutedEventArgs e)
+        {
+            Player.Close();
+            Settings.PageController.ChangeWindow(Pages.WorldMap);
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            Player.Source = DSRPG.Resources.Links.Video.Intro;
+            Player.Volume = Settings.VideoVolume;
+            Player.Stretch = Stretch.Fill;
+            Player.LoadedBehavior = MediaState.Manual;
+            Player.UnloadedBehavior = MediaState.Manual;
+            Player.Play();
+            Window.GetWindow(this).KeyDown += Page_KeyDown;
+            Animation();
+        }
+
+        private async void Animation() 
+        {
+            await Task.Factory.StartNew(() => 
+            {
+                Thread.Sleep(3000);
+                for (double i = 1.0; i > 0.0;i-= 0.01) 
+                {
+                    Dispatcher.Invoke(() => { Skip.Opacity = i; });
+                    Thread.Sleep(50);
+                }
+            });
+        }
+    }
+}
