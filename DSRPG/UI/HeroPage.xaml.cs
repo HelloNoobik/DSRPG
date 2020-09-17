55928e6b7ad716c1457cfa47c5320a8238094d0c
Студент <@5-308-08>
2020-09-17 10:29:22 +0300

    СЛОТЫЫЫЫЫ

--- DSRPG/UI/HeroPage.xaml.cs
+++ DSRPG/UI/HeroPage.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using DSRPG.GameLogic.Mechanics;
 
 namespace DSRPG.UI
 {
@@ -21,7 +22,7 @@ namespace DSRPG.UI
     /// </summary>
     public partial class HeroPage : Page
     {
-        private static DSRPG.GameLogic.Mechanics.Inventory inv = new DSRPG.GameLogic.Mechanics.Inventory();
+        private static Inventory inv = new Inventory();
         public HeroPage()
         {
             InitializeComponent();
@@ -33,43 +34,24 @@ namespace DSRPG.UI
             Point point = new Point(10, 15);
             for (int i = 0; i < inv.GetCountItems(); i++) 
             {
-                Rectangle rect = new Rectangle();
-                Canvas.SetLeft(rect, point.X);
-                Canvas.SetTop(rect, point.Y);
-                rect.Name = $"SlotRect{i}";
-                rect.Height = 50;
-                rect.Width = 50;
-                rect.Stroke = Brushes.White;
-                InventoryWindow.Children.Add(rect);
-                Image img = new Image();
-                Canvas.SetLeft(img, point.X);
-                Canvas.SetTop(img, point.Y);
-                img.Name = $"SlotImg{i}";
-                img.Height = 50;
-                img.Width = 50;
-                img.Source = new BitmapImage(new Uri(inv.GetItem(i).Image,UriKind.Relative));
-                InventoryWindow.Children.Add(img);
-                point.X += rect.Width + 10;
+                Slot slot = new Slot(inv.GetItem(i).Name, new Point(point.X, point.Y), new Size(50, 50), new BitmapImage(new Uri(inv.GetItem(i).Image, UriKind.Relative)), InventoryWindow);
+                point.X += slot.Size.Width + 10;
 
-                rect.MouseEnter += Slot_MouseEnter;
-                rect.MouseLeave += Slot_MouseLeave;
-
-                //img.MouseEnter += Slot_MouseEnter;
-                //img.MouseLeave += Slot_MouseLeave;
-                
+                slot.MouseEnter += Slot_MouseEnter;
+                slot.MouseLeave += Slot_MouseLeave;
 
                 if (point.X == 250)
                 {
                     if (point.Y == InventoryWindow.Height - 65)
                     {
-                        point.Y += 30 + rect.Height;
+                        point.Y += 30 + slot.Size.Height;
                         point.X = 10;
                         InventoryWindow.Height += 200;
                     }
                     else 
                     {
                         point.X = 10;
-                        point.Y += rect.Height + 10;      
+                        point.Y += slot.Size.Height + 10;      
                     }
                 }
             }
@@ -77,14 +59,14 @@ namespace DSRPG.UI
 
         private void Slot_MouseEnter(object sender, MouseEventArgs e) 
         {
-            Rectangle image = (Rectangle)sender;
-            image.Stroke = Brushes.Yellow;
+            Slot slot = (Slot)sender;
+            slot.ChangeBorder(Brushes.Yellow);
         }
 
         private void Slot_MouseLeave(object sender, MouseEventArgs e)
         {
-            Rectangle image = (Rectangle)sender;
-            image.Stroke = Brushes.White;
+            Slot slot = (Slot)sender;
+            slot.ChangeBorder(Brushes.White);
         }
 
         private void next_Click(object sender, RoutedEventArgs e)
